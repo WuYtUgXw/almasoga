e6c9285fcd172c4f9b827bb793c89516ec02e3f9
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-01 17:52:10 +0800

    Update almasoga.sh

--- almasoga.sh
+++ almasoga.sh
@@ -51,9 +51,9 @@ echo -e "${yellow}欢迎使用 涩龙 的 soga 配置脚本！${plain}"
 
 # 提示用户选择功能
 echo -e "\n请选择要执行的功能："
-echo -e "${red}1. 功能一${plain}"
-echo -e "${red}2. 功能二${plain}"
-echo -e "${red}3. 功能三${plain}"
+echo -e "${red}1. 删除审计配置${plain}"
+echo -e "${red}2. 增加审计规则${plain}"
+echo -e "${red}3. 其他功能${plain}"
 
 # 读取用户输入
 read -p "${yellow}请输入功能编号（1、2、3）: ${plain}" function_number
@@ -61,16 +61,56 @@ read -p "${yellow}请输入功能编号（1、2、3）: ${plain}" function_numbe
 # 根据用户输入执行不同的功能
 case $function_number in
     1)
-        echo -e "${red}执行功能一${plain}"
-        # 添加功能一的具体操作
+        echo -e "${red}删除审计配置${plain}"
+        # 删除 /etc/soga/blockList 文件内容
+        echo -n > /etc/soga/blockList
+        echo -e "${green}删除成功${plain}"
         ;;
     2)
-        echo -e "${red}执行功能二${plain}"
-        # 添加功能二的具体操作
+        echo -e "${red}增加审计规则${plain}"
+        # 在 /etc/soga/blockList 文件中添加审计规则
+        cat <<EOF >> /etc/soga/blockList
+# 每行一个审计规则
+# 纯字符串匹配规则
+360.com
+so.cn
+qihoo.com
+360safe.cn
+qhimg.com
+360totalsecurity.cn
+yunpan.com
+
+# 正则表达式匹配规则
+regexp:(.*\\.)(visa|mycard|mastercard|gash|beanfun|bank).*
+regexp:(.*\\.)(metatrader4|metatrader5|mql5).(org|com|net)
+regexp:(Subject|HELO|SMTP)
+regexp:(^.*@)(guerrillamail|guerrillamailblock|sharklasers|grr|pokemail|spam4|bccto|chacuo|027168).(info|biz|com|de|net|org|me|la)
+regexp:(.*\\.)(pincong).(rocks)
+regexp:(.*\\.)(64tianwang|beijingspring|boxun|broadpressinc|chengmingmag|chenpokong|chinaaffairs|chinadigitaltimes|chinesepen|dafahao|dalailamaworld|dalianmeng|dongtaiwang|epochweekly|erabaru|fgmtv|hrichina|huanghuagang|hxwq|jiangweiping|lagranepoca|lantosfoundation|minghui|minzhuzhongguo|ned|ninecommentaries|ogate|renminbao|rfa|secretchina|shenyun|shenyunperformingarts|shenzhoufilm|soundofhope|tiantibooks|tibetpost|truthmoviegroup.wixsite|tuidang|uhrp|uyghuramerican|voachinese|vot|weijingsheng|wujieliulan|xizang-zhiye|zhengjian|zhuichaguoji).(org|com|net)
+regexp:(.*\\.)(gov|12377|12315|talk.news.pts|zhuichaguoji|efcc|cyberpolice|tuidang|nytimes|falundafa|falunaz|mingjingnews|inmediahk|xinsheng|12321|epochweekly|cn.rfi|mingjing|chinaaid|botanwang|xinsheng|rfi|breakgfw|chengmingmag|jinpianwang|xizang-zhiye|breakgfw|qi-gong|voachinese|mhradio|rfa|edoors|ntdtv|epochtimes|cn.nytimes|nytimes|voanews|edoors|renminbao|soundofhope|zhengjian|dafahao|minghui|dongtaiwang|epochtimes|ntdtv|falundafa|wujieliulan|aboluowang|bannedbook|secretchina|dajiyuan|boxun|chinadigitaltimes|huaglad|dwnews|creaders|oneplusnews|rfa|nextdigital|pincong|gtv|kwok7).(cn|com|org|net|club|net|fr|tw|hk|eu|info|me|rocks)
+regexp:(.*\\.)(gov|12377|12315|talk.news.pts.org|creaders|zhuichaguoji|efcc.org|cyberpolice|aboluowang|tuidang|epochtimes|nytimes|dafahao|falundafa|minghui|falunaz|zhengjian|mingjingnews|inmediahk|xinsheng|bannedbook|ntdtv|falungong|12321|secretchina|epochweekly|cn.rfi).(cn|com|org|net|club|net|fr|tw|hk|eu|info|me)
+regexp:(.*\\.)(gov|12377|12315|talk.news.pts.org|cread-ers|zhuich-aguoji|efcc.org|cy-ber-po-lice|abolu-owang|tu-idang|epochtimes|ny-times|zhengjian|mingjingnews|in-medi-ahk|xin-sheng|banned-book|nt-dtv|12321|se-cretchina|epochweekly|cn.rfi).(cn|com|org|net|club|net|fr|tw|hk)
+regexp:(.*\\.)(gash).(com|tw)
+regexp:(.*\\.)(mycard).(com|tw)
+regexp:(.*\\.)(taobao).(com)
+regexp:(.*\\.)(metatrader4|metatrader5|mql5).(org|com|net)
+regexp:(.*\\.)(rising|kingsoft|duba|xindubawukong|jinshanduba).(com|net|org)
+regexp:(torrent|\.torrent|peer_id=|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=)
+regexp:(.?)(xunlei|sandai|Thunder|XLLiveUD)(.)
+regexp:(api|ps|sv|offnavi|newvector|ulog.imap|newloc)(.map|).(baidu|n.shifen).com
+regexp:(.*\\.||)(ipaddress|whatismyipaddress|whoer|iplocation|ip138).(org|com|net|my|to|co)
+.*whatismyip.*
+regexp:(^.*@)(guerrillamail|guerrillamailblock|sharklasers|grr|pokemail|spam4|bccto|chacuo|027168).(info|biz|com|de|net|org|me|la)
+regexp:(ed2k|.torrent|peer_id=|announce|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=|magnet:|xunlei|sandai|Thunder|XLiveUD|bt_key)
+regexp:(.*\\.||)(speed|speedtest|fast|speed.cloudflare).(com|cn|net|co|xyz|dev|edu|pro)
+regexp:(.*\\.||)(.*\\.||)(laomoe|leyunz|jiyou|vv1234|183000|nnan).(com|cn|net|co|xyz|dev|top|cloud|cc)
+regexp:(.*\\.||)(weibo|douyin|douban|tieba.baidu|cctv|dianping|tv.cctv|zhihu|toutiao|xiaohongshu|xhscdn|kuaishou|kuaishouzt).(com|cn|net|co|xyz|dev|edu|pro)
+EOF
+        echo -e "${green}审计规则添加成功${plain}"
         ;;
     3)
-        echo -e "${red}执行功能三${plain}"
-        # 添加功能三的具体操作
+        echo -e "${red}其他功能三${plain}"
+        # 添加其他功能三的具体操作
         ;;
     *)
         echo -e "${red}无效的功能编号${plain}"
