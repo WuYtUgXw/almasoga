fb62192ee9bde568f9954c0e9ef4dd7fa2aba10c
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-01 22:13:02 +0800

    Update almasoga.sh

--- almasoga.sh
+++ almasoga.sh
@@ -62,226 +62,231 @@ else
     fi
 fi
 
-# 安装完成后输出消息
-echo -e "${yellow_light}欢迎使用 涩龙 的 soga 配置脚本！${plain}"
+# 脚本执行完毕
+while true; do
+    # 安装完成后输出消息
+    echo -e "${yellow_light}欢迎使用 涩龙 的 soga 配置脚本！${plain}"
 
-# 检测是否存在 /etc/soga 文件夹
-if [ -d /etc/soga ]; then
-    # 提示用户选择功能
-    echo -e "${red}请选择要执行的功能：${plain}"
-    echo -e "${green}1. Soga配置${plain}"
-    echo -e "${green}2. 解锁配置${plain}"
-    echo -e "${green}3. 审计配置${plain}"
-    echo -e "${green}4. Soga重装${plain}"
-    echo -e "${green}5. 流媒体测试${plain}"
+    # 检测是否存在 /etc/soga 文件夹
+    if [ -d /etc/soga ]; then
+        # 提示用户选择功能
+        echo -e "${red}请选择要执行的功能：${plain}"
+        echo -e "${green}1. Soga配置${plain}"
+        echo -e "${green}2. 解锁配置${plain}"
+        echo -e "${green}3. 审计配置${plain}"
+        echo -e "${green}4. Soga重装${plain}"
+        echo -e "${green}5. 流媒体测试${plain}"
+        echo -e "${green}0. 退出脚本${plain}"
 
-    # 读取用户输入
-    read -p "$(echo -e "${yellow}输入编号: ${plain}")" function_number
+        # 读取用户输入
+        read -p "$(echo -e "${yellow}输入编号: ${plain}")" function_number
 
-    # 根据用户输入执行不同的功能
-    case $function_number in
-        1)
-            echo -e "${green}Soga配置${plain}"
+        # 根据用户输入执行不同的功能
+        case $function_number in
+            1)
+                echo -e "${green}Soga配置${plain}"
 
-            # 提示用户选择操作类型
-            read -p "$(echo -e "${yellow}请选择操作类型：${plain} [1. 文件配置 / 2. 修改配置]: ")" config_option
+                # 提示用户选择操作类型
+                read -p "$(echo -e "${yellow}请选择操作类型：${plain} [1. 文件配置 / 2. 修改配置]: ")" config_option
 
-            case $config_option in
-                1)
-                    echo -e "${green}下载配置文件${plain}"
+                case $config_option in
+                    1)
+                        echo -e "${green}下载配置文件${plain}"
 
-                    # 进入 /etc/soga 下载 https://github.com/WuYtUgXw/almasoga 的 soga.conf 并替换原来的 soga.conf
-                    wget -O /etc/soga/soga.conf https://github.com/WuYtUgXw/almasoga/raw/main/soga.conf
-                    if [ $? -eq 0 ]; then
-                        echo -e "${green}soga.conf 替换成功${plain}"
-                    else
-                        echo -e "${red}soga.conf 替换失败${plain}"
-                        exit 1
-                    fi
-
-                    # 提示输入「编号」，这个编号将在 /etc/soga 的 soga.conf 的 node_id= 后填入
-                    read -p "$(echo -e "${yellow}请输入编号：${plain}")" node_id
-                    sed -i "s/node_id=/node_id=$node_id/" /etc/soga/soga.conf
-
-                    # 保存并重启 soga 服务
-                    systemctl restart soga
-                    if [ $? -eq 0 ]; then
-                        echo -e "${green}Soga配置已更新并服务已重启${plain}"
-                    else
-                        echo -e "${red}Soga服务重启失败${plain}"
-                        exit 1
-                    fi
-                    ;;
-                2)
-                    echo -e "${green}修改配置文件${plain}"
-
-                    # 提示输入「编号」，这个编号将更改 /etc/soga 的 soga.conf 的 node_id=
-                    read -p "$(echo -e "${yellow}请输入新的编号：${plain}")" new_node_id
-                    sed -i "s/node_id=.*/node_id=$new_node_id/" /etc/soga/soga.conf
-
-                    # 保存并重启 soga 服务
-                    systemctl restart soga
-                    if [ $? -eq 0 ]; then
-                        echo -e "${green}Soga配置已更新并服务已重启${plain}"
-                    else
-                        echo -e "${red}Soga服务重启失败${plain}"
-                        exit 1
-                    fi
-                    ;;
-                *)
-                    echo -e "${red}无效的选项${plain}"
-                    exit 1
-                    ;;
-            esac
-            ;;
-        2)
-            echo -e "${green}解锁配置${plain}"
-            
-            # 提示用户选择解锁配置的类型
-            read -p "$(echo -e "${yellow}请选择解锁配置的类型：${plain} [1. DNS解锁 / 2. DNS修改]: ")" unlock_option
-
-            case $unlock_option in
-                1)
-                    echo -e "${green}DNS解锁${plain}"
-
-                    # 下载 https://github.com/WuYtUgXw/almasoga 的 dns.yml 并替换 /etc/soga 原有
-                    wget -O /etc/soga/dns.yml https://github.com/WuYtUgXw/almasoga/raw/main/dns.yml
-                    if [ $? -eq 0 ]; then
-                        echo -e "${green}dns.yml 替换成功${plain}"
-                    else
-                        echo -e "${red}dns.yml 替换失败${plain}"
-                        exit 1
-                    fi
-
-                    # 提示输入「地区缩写」，例如输入「hk」则将 /etc/soga 的 dns.yml 里的 sin.core 修改为 hkg.core
-                    read -p "$(echo -e "${yellow}请输入地区缩写：${plain}")" region_abbr
-
-                    case $region_abbr in
-                        hk)
-                            sed -i "s/sin.core/hkg.core/" /etc/soga/dns.yml
-                            ;;
-                        sg)
-                            sed -i "s/sin.core/sin.core/" /etc/soga/dns.yml
-                            ;;
-                        jp)
-                            sed -i "s/sin.core/nrt.core/" /etc/soga/dns.yml
-                            ;;
-                        us)
-                            sed -i "s/sin.core/lax.core/" /etc/soga/dns.yml
-                            ;;
-                        *)
-                            echo -e "${red}无效的地区缩写${plain}"
+                        # 进入 /etc/soga 下载 https://github.com/WuYtUgXw/almasoga 的 soga.conf 并替换原来的 soga.conf
+                        wget -O /etc/soga/soga.conf https://github.com/WuYtUgXw/almasoga/raw/main/soga.conf
+                        if [ $? -eq 0 ]; then
+                            echo -e "${green}soga.conf 替换成功${plain}"
+                        else
+                            echo -e "${red}soga.conf 替换失败${plain}"
                             exit 1
-                            ;;
-                    esac
+                        fi
 
-                    echo -e "${green}DNS解锁配置已完成${plain}"
-                    ;;
-                2)
-                    echo -e "${green}DNS修改${plain}"
+                        # 提示输入「编号」，这个编号将在 /etc/soga 的 soga.conf 的 node_id= 后填入
+                        read -p "$(echo -e "${yellow}请输入编号：${plain}")" node_id
+                        sed -i "s/node_id=/node_id=$node_id/" /etc/soga/soga.conf
 
-                    # 提示输入「地区缩写」，例如输入「hk」则将 /etc/soga 的 dns.yml 里的 sin.core 修改为 hkg.core
-                    read -p "$(echo -e "${yellow}请输入地区缩写：${plain}")" region_abbr
-
-                    case $region_abbr in
-                        hk)
-                            replace_rules="s/sin.core/hkg.core/;s/hkg.core/hkg.core/;s/nrt.core/hkg.core/;s/lax.core/hkg.core/;"
-                            ;;
-                        sg)
-                            replace_rules="s/sin.core/sin.core/;s/hkg.core/sin.core/;s/nrt.core/sin.core/;s/lax.core/sin.core/;"
-                            ;;
-                        jp)
-                            replace_rules="s/sin.core/nrt.core/;s/hkg.core/nrt.core/;s/nrt.core/nrt.core/;s/lax.core/nrt.core/;"
-                            ;;
-                        us)
-                            replace_rules="s/sin.core/lax.core/;s/hkg.core/lax.core/;s/nrt.core/lax.core/;s/lax.core/lax.core/;"
-                            ;;
-                        *)
-                            echo -e "${red}无效的地区缩写: $region_abbr${plain}"
+                        # 保存并重启 soga 服务
+                        systemctl restart soga
+                        if [ $? -eq 0 ]; then
+                            echo -e "${green}Soga配置已更新并服务已重启${plain}"
+                        else
+                            echo -e "${red}Soga服务重启失败${plain}"
                             exit 1
-                            ;;
-                    esac
+                        fi
+                        ;;
+                    2)
+                        echo -e "${green}修改配置文件${plain}"
 
-                    # 应用替换规则
-                    sed -i "${replace_rules}" /etc/soga/dns.yml
+                        # 提示输入「编号」，这个编号将更改 /etc/soga 的 soga.conf 的 node_id=
+                        read -p "$(echo -e "${yellow}请输入新的编号：${plain}")" new_node_id
+                        sed -i "s/node_id=.*/node_id=$new_node_id/" /etc/soga/soga.conf
 
-                    echo -e "${green}DNS修改配置已完成${plain}"
-                    ;;
-                *)
-                    echo -e "${red}无效的选项${plain}"
-                    exit 1
-                    ;;
-            esac
-            ;;
-        3)
-            echo -e "${green}审计配置${plain}"
-            
-            # 提示用户选择操作类型
-            read -p "$(echo -e "${yellow}请选择操作类型：${plain} [1. 删除审计 / 2. 增加审计]: ")" audit_option
+                        # 保存并重启 soga 服务
+                        systemctl restart soga
+                        if [ $? -eq 0 ]; then
+                            echo -e "${green}Soga配置已更新并服务已重启${plain}"
+                        else
+                            echo -e "${red}Soga服务重启失败${plain}"
+                            exit 1
+                        fi
+                        ;;
+                    *)
+                        echo -e "${red}无效的选项${plain}"
+                        exit 1
+                        ;;
+                esac
+                ;;
+            2)
+                echo -e "${green}解锁配置${plain}"
+                
+                # 提示用户选择解锁配置的类型
+                read -p "$(echo -e "${yellow}请选择解锁配置的类型：${plain} [1. DNS解锁 / 2. DNS修改]: ")" unlock_option
 
-            case $audit_option in
-                1)
-                    echo -e "${green}删除审计${plain}"
-                    # 删除 /etc/soga/blockList 文件内容
-                    echo -n > /etc/soga/blockList
+                case $unlock_option in
+                    1)
+                        echo -e "${green}DNS解锁${plain}"
+
+                        # 下载 https://github.com/WuYtUgXw/almasoga 的 dns.yml 并替换 /etc/soga 原有的 dns.yml
+                        wget -O /etc/soga/dns.yml https://github.com/WuYtUgXw/almasoga/raw/main/dns.yml
+                        if [ $? -eq 0 ]; then
+                            echo -e "${green}dns.yml 替换成功${plain}"
+                        else
+                            echo -e "${red}dns.yml 替换失败${plain}"
+                            exit 1
+                        fi
+
+                        # 提示输入「地区缩写」，例如输入「hk」则将 /etc/soga 的 dns.yml 里的 sin.core 修改为 hkg.core
+                        read -p "$(echo -e "${yellow}请输入地区缩写：${plain}")" region_abbr
+
+                        case $region_abbr in
+                            hk)
+                                sed -i "s/sin.core/hkg.core/" /etc/soga/dns.yml
+                                ;;
+                            sg)
+                                sed -i "s/sin.core/sin.core/" /etc/soga/dns.yml
+                                ;;
+                            jp)
+                                sed -i "s/sin.core/nrt.core/" /etc/soga/dns.yml
+                                ;;
+                            us)
+                                sed -i "s/sin.core/lax.core/" /etc/soga/dns.yml
+                                ;;
+                            *)
+                                echo -e "${red}无效的地区缩写${plain}"
+                                exit 1
+                                ;;
+                        esac
+
+                        echo -e "${green}DNS解锁配置已完成${plain}"
+                        ;;
+                    2)
+                        echo -e "${green}DNS修改${plain}"
+
+                        # 提示输入「地区缩写」，例如输入「hk」则将 /etc/soga 的 dns.yml 里的 sin.core 修改为 hkg.core
+                        read -p "$(echo -e "${yellow}请输入地区缩写：${plain}")" region_abbr
+
+                        case $region_abbr in
+                            hk)
+                                replace_rules="s/sin.core/hkg.core/;s/hkg.core/hkg.core/;s/nrt.core/hkg.core/;s/lax.core/hkg.core/;"
+                                ;;
+                            sg)
+                                replace_rules="s/sin.core/sin.core/;s/hkg.core/sin.core/;s/nrt.core/sin.core/;s/lax.core/sin.core/;"
+                                ;;
+                            jp)
+                                replace_rules="s/sin.core/nrt.core/;s/hkg.core/nrt.core/;s/nrt.core/nrt.core/;s/lax.core/nrt.core/;"
+                                ;;
+                            us)
+                                replace_rules="s/sin.core/lax.core/;s/hkg.core/lax.core/;s/nrt.core/lax.core/;s/lax.core/lax.core/;"
+                                ;;
+                            *)
+                                echo -e "${red}无效的地区缩写: $region_abbr${plain}"
+                                exit 1
+                                ;;
+                        esac
+
+                        # 应用替换规则
+                        sed -i "${replace_rules}" /etc/soga/dns.yml
+
+                        echo -e "${green}DNS修改配置已完成${plain}"
+                        ;;
+                    *)
+                        echo -e "${red}无效的选项${plain}"
+                        exit 1
+                        ;;
+                esac
+                ;;
+            3)
+                echo -e "${green}审计配置${plain}"
+                
+                # 提示用户选择操作类型
+                read -p "$(echo -e "${yellow}请选择操作类型：${plain} [1. 删除审计 / 2. 增加审计]: ")" audit_option
+
+                case $audit_option in
+                    1)
+                        echo -e "${green}删除审计${plain}"
+                        # 删除 /etc/soga/blockList 文件内容
+                        echo -n > /etc/soga/blockList
+                        if [ $? -eq 0 ]; then
+                            echo -e "${green}删除成功${plain}"
+                        else
+                            echo -e "${red}删除失败${plain}"
+                            exit 1
+                        fi
+                        ;;
+                    2)
+                        echo -e "${green}增加审计${plain}"
+                        # 下载 https://github.com/WuYtUgXw/almasoga 的 blockList 并替换 /etc/soga 原有的 blockList
+                        wget -O /etc/soga/blockList https://github.com/WuYtUgXw/almasoga/raw/main/blockList
+                        if [ $? -eq 0 ]; then
+                            echo -e "${green}blockList 替换成功${plain}"
+                        else
+                            echo -e "${red}blockList 替换失败${plain}"
+                            exit 1
+                        fi
+                        ;;
+                    *)
+                        echo -e "${green}无效的选项${plain}"
+                        exit 1
+                        ;;
+                esac
+                ;;
+            4)
+                echo -e "${green}Soga重装${plain}"
+                
+                # Prompt for confirmation
+                read -p "$(echo -e "${yellow}您确定要重新安装 Soga 吗？ (yes/no): ${plain}")" reinstall_choice
+
+                if [ "$reinstall_choice" == "yes" ] || [ "$reinstall_choice" == "y" ]; then
+                    echo -e "${yellow_light}正在删除 /etc/soga...${plain}"
+                    rm -rf /etc/soga
+
+                    # Proceed with the installation
+                    echo -e "${green}/etc/soga 文件夹已删除，开始重新安装 soga${plain}"
+                    bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/soga/master/install.sh)
                     if [ $? -eq 0 ]; then
-                        echo -e "${green}删除成功${plain}"
+                        echo -e "${green}soga 重新安装成功${plain}"
                     else
-                        echo -e "${red}删除失败${plain}"
+                        echo -e "${red}soga 安装失败${plain}"
                         exit 1
                     fi
-                    ;;
-                2)
-                    echo -e "${green}增加审计${plain}"
-                    # 下载 https://github.com/WuYtUgXw/almasoga 的 blockList 并替换 /etc/soga 原有的 blockList
-                    wget -O /etc/soga/blockList https://github.com/WuYtUgXw/almasoga/raw/main/blockList
-                    if [ $? -eq 0 ]; then
-                        echo -e "${green}blockList 替换成功${plain}"
-                    else
-                        echo -e "${red}blockList 替换失败${plain}"
-                        exit 1
-                    fi
-                    ;;
-                *)
-                    echo -e "${green}无效的选项${plain}"
-                    exit 1
-                    ;;
-            esac
-            ;;
-        4)
-            echo -e "${green}Soga重装${plain}"
-            
-            # Prompt for confirmation
-            read -p "$(echo -e "${yellow}您确定要重新安装 Soga 吗？ (yes/no): ${plain}")" reinstall_choice
-
-            if [ "$reinstall_choice" == "yes" ] || [ "$reinstall_choice" == "y" ]; then
-                echo -e "${yellow_light}正在删除 /etc/soga...${plain}"
-                rm -rf /etc/soga
-
-                # Proceed with the installation
-                echo -e "${green}/etc/soga 文件夹已删除，开始重新安装 soga${plain}"
-                bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/soga/master/install.sh)
-                if [ $? -eq 0 ]; then
-                    echo -e "${green}soga 重新安装成功${plain}"
                 else
-                    echo -e "${red}soga 安装失败${plain}"
-                    exit 1
+                    echo -e "${yellow_light}Soga 重装已取消${plain}"
+                    exit 0
                 fi
-            else
-                echo -e "${yellow_light}Soga 重装已取消${plain}"
+                ;;
+            5)
+                echo -e "${green}流媒体测试${plain}"
+                bash <(curl -L -s https://netflix.dad/detect-script)
+                ;;
+            0)
+                echo -e "${yellow_light}退出脚本${plain}"
                 exit 0
-            fi
-            ;;
-        5)
-            echo -e "${green}流媒体测试${plain}"
-            bash <(curl -L -s https://netflix.dad/detect-script)
-            ;;
-        *)
-            echo -e "${green}无效的操作编号${plain}"
-            exit 1
-            ;;
-    esac
-
-    # 脚本执行完毕
-    exit 0
-fi
+                ;;
+            *)
+                echo -e "${green}无效的操作编号${plain}"
+                exit 1
+                ;;
+        esac
+    fi
+done
