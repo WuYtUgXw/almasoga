9fb7cd1c6a5b74250f3e43b77fc9910f6cf4d275
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-01 20:31:10 +0800

    Update almasoga.sh

--- almasoga.sh
+++ almasoga.sh
@@ -65,127 +65,124 @@ fi
 # 安装完成后输出消息
 echo -e "${yellow_light}欢迎使用 涩龙 的 soga 配置脚本！${plain}"
 
-# 提示用户选择功能
-echo -e "${red}请选择要执行的功能：${plain}"
-echo -e "${green}1. Soga配置${plain}"
-echo -e "${green}2. 解锁配置${plain}"
-echo -e "${green}3. 审计配置${plain}"
+# 检测是否存在 /etc/soga 文件夹
+if [ -d /etc/soga ]; then
+    # 提示用户选择功能
+    echo -e "${red}请选择要执行的功能：${plain}"
+    echo -e "${green}1. Soga配置${plain}"
+    echo -e "${green}2. 解锁配置${plain}"
+    echo -e "${green}3. 审计配置${plain}"
 
-# 读取用户输入
-read -p "$(echo -e "${yellow}输入编号: ${plain}")" function_number
+    # 读取用户输入
+    read -p "$(echo -e "${yellow}输入编号: ${plain}")" function_number
 
-# 根据用户输入执行不同的功能
-case $function_number in
-    1)
-        echo -e "${green}Soga配置${plain}"
+    # 根据用户输入执行不同的功能
+    case $function_number in
+        1)
+            echo -e "${green}Soga配置${plain}"
 
-        # 提示用户选择操作类型
-        if [ -d /etc/soga ]; then
-            # 如果已安装，则直接选择文件配置
-            config_option=1
-        else
-            # 如果未安装，则提醒用户选择操作类型
+            # 提示用户选择操作类型
             read -p "$(echo -e "${yellow}请选择操作类型：${plain} [1. 文件配置 / 2. 修改配置]: ")" config_option
-        fi
 
-        case $config_option in
-            1)
-                echo -e "${green}下载配置文件${plain}"
+            case $config_option in
+                1)
+                    echo -e "${green}下载配置文件${plain}"
 
-                # 进入 /etc/soga 下载 https://github.com/WuYtUgXw/almasoga 的 soga.conf 并替换原来的 soga.conf
-                wget -O /etc/soga/soga.conf https://github.com/WuYtUgXw/almasoga/raw/main/soga.conf
-                if [ $? -eq 0 ]; then
-                    echo -e "${green}soga.conf 替换成功${plain}"
-                else
-                    echo -e "${red}soga.conf 替换失败${plain}"
+                    # 进入 /etc/soga 下载 https://github.com/WuYtUgXw/almasoga 的 soga.conf 并替换原来的 soga.conf
+                    wget -O /etc/soga/soga.conf https://github.com/WuYtUgXw/almasoga/raw/main/soga.conf
+                    if [ $? -eq 0 ]; then
+                        echo -e "${green}soga.conf 替换成功${plain}"
+                    else
+                        echo -e "${red}soga.conf 替换失败${plain}"
+                        exit 1
+                    fi
+
+                    # 提示输入「编号」，这个编号将在 /etc/soga 的 soga.conf 的 node_id= 后填入
+                    read -p "$(echo -e "${yellow}请输入编号：${plain}")" node_id
+                    sed -i "s/node_id=/node_id=$node_id/" /etc/soga/soga.conf
+
+                    # 保存并重启 soga 服务
+                    systemctl restart soga
+                    if [ $? -eq 0 ]; then
+                        echo -e "${green}Soga配置已更新并服务已重启${plain}"
+                    else
+                        echo -e "${red}Soga服务重启失败${plain}"
+                        exit 1
+                    fi
+                    ;;
+                2)
+                    echo -e "${green}修改配置文件${plain}"
+
+                    # 提示输入「编号」，这个编号将更改 /etc/soga 的 soga.conf 的 node_id=
+                    read -p "$(echo -e "${yellow}请输入新的编号：${plain}")" new_node_id
+                    sed -i "s/node_id=.*/node_id=$new_node_id/" /etc/soga/soga.conf
+
+                    # 保存并重启 soga 服务
+                    systemctl restart soga
+                    if [ $? -eq 0 ]; then
+                        echo -e "${green}Soga配置已更新并服务已重启${plain}"
+                    else
+                        echo -e "${red}Soga服务重启失败${plain}"
+                        exit 1
+                    fi
+                    ;;
+                *)
+                    echo -e "${red}无效的选项${plain}"
                     exit 1
-                fi
-
-                # 提示输入「编号」，这个编号将在 /etc/soga 的 soga.conf 的 node_id= 后填入
-                read -p "$(echo -e "${yellow}请输入编号：${plain}")" node_id
-                sed -i "s/node_id=/node_id=$node_id/" /etc/soga/soga.conf
-
-                # 保存并重启 soga 服务
-                systemctl restart soga
-                if [ $? -eq 0 ]; then
-                    echo -e "${green}Soga配置已更新并服务已重启${plain}"
-                else
-                    echo -e "${red}Soga服务重启失败${plain}"
-                    exit 1
-                fi
-                ;;
-            2)
-                echo -e "${green}修改配置文件${plain}"
-
-                # 提示输入「编号」，这个编号将更改 /etc/soga 的 soga.conf 的 node_id=
-                read -p "$(echo -e "${yellow}请输入新的编号：${plain}")" new_node_id
-                sed -i "s/node_id=.*/node_id=$new_node_id/" /etc/soga/soga.conf
-
-                # 保存并重启 soga 服务
-                systemctl restart soga
-                if [ $? -eq 0 ]; then
-                    echo -e "${green}Soga配置已更新并服务已重启${plain}"
-                else
-                    echo -e "${red}Soga服务重启失败${plain}"
-                    exit 1
-                fi
-                ;;
-            *)
-                echo -e "${red}无效的选项${plain}"
+                    ;;
+            esac
+            ;;
+        2)
+            echo -e "${green}解锁配置${plain}"
+            # 下载 https://github.com/WuYtUgXw/almasoga 的配置文件并替换 /etc/soga 原有的配置
+            wget -O /etc/soga/soga.conf https://github.com/WuYtUgXw/almasoga/raw/main/soga.conf
+            if [ $? -eq 0 ]; then
+                echo -e "${green}解锁配置替换成功${plain}"
+            else
+                echo -e "${red}解锁配置替换失败${plain}"
                 exit 1
-                ;;
-        esac
-        ;;
-    2)
-        echo -e "${green}解锁配置${plain}"
-        # 下载 https://github.com/WuYtUgXw/almasoga 的配置文件并替换 /etc/soga 原有的配置
-        wget -O /etc/soga/soga.conf https://github.com/WuYtUgXw/almasoga/raw/main/soga.conf
-        if [ $? -eq 0 ]; then
-            echo -e "${green}解锁配置替换成功${plain}"
-        else
-            echo -e "${red}解锁配置替换失败${plain}"
+            fi
+            ;;
+        3)
+            echo -e "${green}审计配置${plain}"
+            # 提示用户选择操作类型
+            read -p "$(echo -e "${yellow}请选择操作类型：${plain} [1. 删除审计 / 2. 增加审计]: ")" audit_option
+
+            case $audit_option in
+                1)
+                    echo -e "${green}删除审计${plain}"
+                    # 删除 /etc/soga/blockList 文件内容
+                    echo -n > /etc/soga/blockList
+                    if [ $? -eq 0 ]; then
+                        echo -e "${green}删除成功${plain}"
+                    else
+                        echo -e "${red}删除失败${plain}"
+                        exit 1
+                    fi
+                    ;;
+                2)
+                    echo -e "${green}增加审计${plain}"
+                    # 下载 https://github.com/WuYtUgXw/almasoga 的 blockList 并替换 /etc/soga 原有的 blockList
+                    wget -O /etc/soga/blockList https://github.com/WuYtUgXw/almasoga/raw/main/blockList
+                    if [ $? -eq 0 ]; then
+                        echo -e "${green}blockList 替换成功${plain}"
+                    else
+                        echo -e "${red}blockList 替换失败${plain}"
+                        exit 1
+                    fi
+                    ;;
+                *)
+                    echo -e "${green}无效的选项${plain}"
+                    exit 1
+                    ;;
+            esac
+            ;;
+        *)
+            echo -e "${green}无效的操作编号${plain}"
             exit 1
-        fi
-        ;;
-    3)
-        echo -e "${green}审计配置${plain}"
-        # 提示用户选择操作类型
-        read -p "$(echo -e "${yellow}请选择操作类型：${plain} [1. 删除审计 / 2. 增加审计]: ")" audit_option
+            ;;
+    esac
 
-        case $audit_option in
-            1)
-                echo -e "${green}删除审计${plain}"
-                # 删除 /etc/soga/blockList 文件内容
-                echo -n > /etc/soga/blockList
-                if [ $? -eq 0 ]; then
-                    echo -e "${green}删除成功${plain}"
-                else
-                    echo -e "${red}删除失败${plain}"
-                    exit 1
-                fi
-                ;;
-            2)
-                echo -e "${green}增加审计${plain}"
-                # 下载 https://github.com/WuYtUgXw/almasoga 的 blockList 并替换 /etc/soga 原有的 blockList
-                wget -O /etc/soga/blockList https://github.com/WuYtUgXw/almasoga/raw/main/blockList
-                if [ $? -eq 0 ]; then
-                    echo -e "${green}blockList 替换成功${plain}"
-                else
-                    echo -e "${red}blockList 替换失败${plain}"
-                    exit 1
-                fi
-                ;;
-            *)
-                echo -e "${green}无效的选项${plain}"
-                exit 1
-                ;;
-        esac
-        ;;
-    *)
-        echo -e "${green}无效的操作编号${plain}"
-        exit 1
-        ;;
-esac
-
-# 脚本执行完毕
-exit 0
+    # 脚本执行完毕
+    exit 0
+fi
