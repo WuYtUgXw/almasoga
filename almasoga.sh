b9ca40ffe4b58081859bc4ffea75a67200622c8c
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-01 17:41:17 +0800

    Update almasoga.sh

--- almasoga.sh
+++ almasoga.sh
@@ -36,19 +36,49 @@ if [ -f /etc/debian_version ]; then
 fi
 
 # 检测是否存在 /etc/soga 文件夹
-if [ ! -d /etc/soga ]; then
-    # 不存在 /etc/soga 文件夹
-    echo -e "${yellow}/etc/soga 文件夹不存在，开始安装 soga${plain}"
-    
-    # 运行 soga 安装脚本
-    bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/soga/master/install.sh)
-
-    # 安装完成后输出消息
-    echo -e "${green}soga 安装完成${plain}"
-else
-    # 存在 /etc/soga 文件夹
-    echo -e "${green}/etc/soga 文件夹已存在，跳过 soga 安装${plain}"
+if [ -d /etc/soga ]; then
+    # 存在 /etc/soga 文件夹，删除
+    echo -e "${yellow}/etc/soga 文件夹已存在，删除后重新安装 soga${plain}"
+    rm -rf /etc/soga
 fi
+
+# 不存在 /etc/soga 文件夹，开始安装 soga
+echo -e "${yellow}/etc/soga 文件夹不存在，开始安装 soga${plain}"
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/soga/master/install.sh)
+
+# 安装完成后输出消息
+echo -e "${green}soga 安装完成${plain}"
+
+# 显示欢迎消息
+echo -e "\n欢迎使用 Damian 的 soga 配置脚本！"
+
+# 提示用户选择功能
+echo -e "\n请选择要执行的功能："
+echo -e "1. 功能一"
+echo -e "2. 功能二"
+echo -e "3. 功能三"
+
+# 读取用户输入
+read -p "请输入功能编号（1、2、3）: " function_number
+
+# 根据用户输入执行不同的功能
+case $function_number in
+    1)
+        echo -e "执行功能一"
+        # 添加功能一的具体操作
+        ;;
+    2)
+        echo -e "执行功能二"
+        # 添加功能二的具体操作
+        ;;
+    3)
+        echo -e "执行功能三"
+        # 添加功能三的具体操作
+        ;;
+    *)
+        echo -e "${red}无效的功能编号${plain}"
+        ;;
+esac
 
 # 脚本执行完毕
 exit 0
