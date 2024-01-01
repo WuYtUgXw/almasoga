d01dc791b44539b6506bd871870baa4588631e37
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-01 18:43:45 +0800

    Update almasoga.sh

--- almasoga.sh
+++ almasoga.sh
@@ -66,9 +66,9 @@ echo -e "${yellow}欢迎使用 Damian 的 soga 配置脚本！${plain}"
 
 # 提示用户选择功能
 echo -e "\n请选择要执行的功能："
-echo -e "${red}1. 审计配置${plain}"
-echo -e "${red}2. 替换为 https://github.com/WuYtUgXw/almasoga 的 blockList${plain}"
-echo -e "${red}3. 其他功能${plain}"
+echo -e "${red}1. Soga配置${plain}"
+echo -e "${red}2. 解锁配置${plain}"
+echo -e "${red}3. 审计配置${plain}"
 
 # 读取用户输入
 read -p "$(echo -e "${yellow}请输入功能编号（1、2、3）: ${plain}")" function_number
@@ -76,37 +76,35 @@ read -p "$(echo -e "${yellow}请输入功能编号（1、2、3）: ${plain}")" f
 # 根据用户输入执行不同的功能
 case $function_number in
     1)
+        echo -e "${red}Soga配置${plain}"
+        # Soga配置的具体操作
+        ;;
+    2)
+        echo -e "${red}解锁配置${plain}"
+        # 下载 https://github.com/WuYtUgXw/almasoga 的配置文件并替换 /etc/soga 原有的配置
+        wget -O /etc/soga/soga.conf https://github.com/WuYtUgXw/almasoga/raw/main/soga.conf
+        echo -e "${green}解锁配置替换成功${plain}"
+        ;;
+    3)
         echo -e "${red}审计配置${plain}"
         # 提示用户选择操作类型
-        read -p "$(echo -e "${red}请选择操作类型：${plain} [1. 删除审计规则 / 2. 增加审计规则]: ")" audit_option
+        read -p "$(echo -e "${red}请选择操作类型：${plain} [1. 添加审计规则 / 2. 删除审计规则]: ")" audit_option
 
         case $audit_option in
             1)
+                echo -e "${red}添加审计规则${plain}"
+                # 添加审计规则的具体操作
+                ;;
+            2)
                 echo -e "${red}删除审计规则${plain}"
                 # 删除 /etc/soga/blockList 文件内容
                 echo -n > /etc/soga/blockList
                 echo -e "${green}删除成功${plain}"
                 ;;
-            2)
-                echo -e "${red}增加审计规则${plain}"
-                # 下载 https://github.com/WuYtUgXw/almasoga 的 blockList 并替换 /etc/soga 原有的 blockList
-                wget -O /etc/soga/blockList https://github.com/WuYtUgXw/almasoga/raw/main/blockList
-                echo -e "${green}blockList 替换成功${plain}"
-                ;;
             *)
                 echo -e "${red}无效的选项${plain}"
                 ;;
         esac
-        ;;
-    2)
-        echo -e "${red}增加审计配置${plain}"
-        # 下载 https://github.com/WuYtUgXw/almasoga 的 blockList 并替换 /etc/soga 原有的 blockList
-        wget -O /etc/soga/blockList https://github.com/WuYtUgXw/almasoga/raw/main/blockList
-        echo -e "${green}blockList 替换成功${plain}"
-        ;;
-    3)
-        echo -e "${red}其他功能，暂未实现${plain}"
-        # 添加其他功能的具体操作
         ;;
     *)
         echo -e "${red}无效的操作编号${plain}"
