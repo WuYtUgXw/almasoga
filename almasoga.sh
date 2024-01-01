c71c1646f8f5bb13c6b794fde4c77a13724a954b
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-01 22:46:07 +0800

    Update almasoga.sh

--- almasoga.sh
+++ almasoga.sh
@@ -6,7 +6,7 @@ green='\033[1;32m'
 yellow='\033[1;33m'
 yellow_light='\033[2;36m'
 plain='\033[0m'
-
+ 
 # 检测是否存在 /etc/soga 文件夹
 if [ -d /etc/soga ]; then 
     echo -e "${green}/etc/soga 文件夹已存在，跳过安装 soga${plain}"
