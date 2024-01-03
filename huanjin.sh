3bf301d2aadafa586eacd573ee925d9662744ec4
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-03 22:00:44 +0800

    Update huanjin.sh

--- huanjin.sh
+++ huanjin.sh
@@ -14,76 +14,81 @@ function detect_system {
         elif [[ $ID == "centos" || $ID == "fedora" || $ID == "rhel" ]]; then
             SYSTEM_TYPE="centos"
         else
-            echo -e "${RED}不支持的系统类型: $ID${NC}"
+            echo -e "${RED}Unsupported system type: $ID${NC}"
             exit 1
         fi
     else
-        echo -e "${RED}无法读取 /etc/os-release 文件，无法确定系统类型${NC}"
+        echo -e "${RED}Unable to read /etc/os-release file, cannot determine system type${NC}"
         exit 1
     fi
 }
 
 function main_menu {
-    echo -e "${YELLOW}欢迎使用环境配置脚本！${NC}"
-    echo -e "${YELLOW}1: ${WHITE}wget配置${NC}"
-    echo -e "${YELLOW}2: ${WHITE}Docker配置${NC}"
-    echo -e "${YELLOW}3: ${WHITE}宝塔配置${NC}"
-    echo -e "${YELLOW}4: ${WHITE}TCP调优${NC}"
-    echo -e "${YELLOW}5: ${WHITE}Curl配置${NC}"
-    echo -e "${YELLOW}6: ${WHITE}BBR3配置${NC}"
-    echo -e "${YELLOW}7: ${WHITE}禁用IPv6网络${NC}"
-    echo -e "${YELLOW}0: ${WHITE}退出脚本${NC}"
+    echo -e "${YELLOW}Welcome to the environment configuration script!${NC}"
+    echo -e "${YELLOW}1: ${WHITE}wget configuration${NC}"
+    echo -e "${YELLOW}2: ${WHITE}Docker configuration${NC}"
+    echo -e "${YELLOW}3: ${WHITE}Baota configuration${NC}"
+    echo -e "${YELLOW}4: ${WHITE}TCP tuning${NC}"
+    echo -e "${YELLOW}5: ${WHITE}Curl configuration${NC}"
+    echo -e "${YELLOW}6: ${WHITE}BBR3 configuration${NC}"
+    echo -e "${YELLOW}7: ${WHITE}Disable IPv6 network${NC}"
+    echo -e "${YELLOW}0: ${WHITE}Exit script${NC}"
 }
 
 function wget_menu {
-    echo -e "${YELLOW}1: ${WHITE}安装${NC}"
-    echo -e "${YELLOW}2: ${WHITE}卸载${NC}"
-    echo -e "${YELLOW}3: ${WHITE}返回上级菜单${NC}"
+    echo -e "${YELLOW}1: ${WHITE}Install${NC}"
+    echo -e "${YELLOW}2: ${WHITE}Uninstall${NC}"
+    echo -e "${YELLOW}3: ${WHITE}Return to the main menu${NC}"
 }
 
 function docker_menu {
-    echo -e "${YELLOW}1: ${WHITE}安装${NC}"
-    echo -e "${YELLOW}2: ${WHITE}卸载${NC}"
-    echo -e "${YELLOW}3: ${WHITE}返回上级菜单${NC}"
+    echo -e "${YELLOW}1: ${WHITE}Install${NC}"
+    echo -e "${YELLOW}2: ${WHITE}Uninstall${NC}"
+    echo -e "${YELLOW}3: ${WHITE}Return to the main menu${NC}"
 }
 
 function bt_menu {
-    echo -e "${YELLOW}1: ${WHITE}安装中文宝塔${NC}"
-    echo -e "${YELLOW}2: ${WHITE}安装国际宝塔${NC}"
-    echo -e "${YELLOW}3: ${WHITE}卸载宝塔${NC}"
-    echo -e "${YELLOW}0: ${WHITE}返回上级菜单${NC}"
+    echo -e "${YELLOW}1: ${WHITE}Install Chinese Baota${NC}"
+    echo -e "${YELLOW}2: ${WHITE}Install International Baota${NC}"
+    echo -e "${YELLOW}3: ${WHITE}Uninstall Baota${NC}"
+    echo -e "${YELLOW}0: ${WHITE}Return to the main menu${NC}"
 }
 
 function tcp_tuning_menu {
-    echo -e "${YELLOW}1: ${WHITE}运行 TCP调优脚本${NC}"
-    echo -e "${YELLOW}2: ${WHITE}返回上级菜单${NC}"
+    echo -e "${YELLOW}1: ${WHITE}Run TCP tuning script${NC}"
+    echo -e "${YELLOW}2: ${WHITE}Return to the main menu${NC}"
 }
 
 function curl_menu {
-    echo -e "${YELLOW}1: ${WHITE}安装${NC}"
-    echo -e "${YELLOW}2: ${WHITE}卸载${NC}"
-    echo -e "${YELLOW}3: ${WHITE}返回上级菜单${NC}"
+    echo -e "${YELLOW}1: ${WHITE}Install${NC}"
+    echo -e "${YELLOW}2: ${WHITE}Uninstall${NC}"
+    echo -e "${YELLOW}3: ${WHITE}Return to the main menu${NC}"
 }
 
 function bbr3_menu {
-    echo -e "${YELLOW}1: ${WHITE}安装${NC}"
-    echo -e "${YELLOW}2: ${WHITE}备份${NC}"
-    echo -e "${YELLOW}3: ${WHITE}还原${NC}"
-    echo -e "${YELLOW}4: ${WHITE}卸载${NC}"
-    echo -e "${YELLOW}5: ${WHITE}返回上级菜单${NC}"
+    echo -e "${YELLOW}1: ${WHITE}Install${NC}"
+    echo -e "${YELLOW}2: ${WHITE}Restore${NC}"
+    echo -e "${YELLOW}3: ${WHITE}Uninstall${NC}"
+    echo -e "${YELLOW}4: ${WHITE}Return to the main menu${NC}"
+}
+
+function disable_ipv6_menu {
+    echo -e "${YELLOW}1: ${WHITE}Disable IPv6${NC}"
+    echo -e "${YELLOW}2: ${WHITE}Enable IPv6${NC}"
+    echo -e "${YELLOW}0: ${WHITE}Return to the main menu${NC}"
 }
 
 detect_system
 
 while true; do
     main_menu
-    read -p "请选择一级选项 (输入对应数字): " main_choice
+    read -p "Select a main option (enter the corresponding number): " main_choice
 
     case $main_choice in
         1)
             while true; do
                 wget_menu
-                read -p "请选择 wget 配置选项 (输入对应数字): " wget_choice
+                read -p "Select a wget configuration option (enter the corresponding number): " wget_choice
                 case $wget_choice in
                     1)
                         if [[ $SYSTEM_TYPE == "debian" ]]; then
@@ -104,7 +109,7 @@ while true; do
                         break
                         ;;
                     *)
-                        echo -e "${RED}无效的选择${NC}"
+                        echo -e "${RED}Invalid choice${NC}"
                         ;;
                 esac
             done
@@ -112,7 +117,7 @@ while true; do
         2)
             while true; do
                 docker_menu
-                read -p "请选择 Docker 配置选项 (输入对应数字): " docker_choice
+                read -p "Select a Docker configuration option (enter the corresponding number): " docker_choice
                 case $docker_choice in
                     1)
                         if [[ $SYSTEM_TYPE == "debian" ]]; then
@@ -133,168 +138,7 @@ while true; do
                         break
                         ;;
                     *)
-                        echo -e "${RED}无效的选择${NC}"
+                        echo -e "${RED}Invalid choice${NC}"
                         ;;
                 esac
-            done
-            ;;
-        3)
-            while true; do
-                bt_menu
-                read -p "请选择宝塔配置选项 (输入对应数字): " bt_choice
-                case $bt_choice in
-                    1)
-                        if [[ $SYSTEM_TYPE == "debian" ]]; then
-                            sudo apt-get install -y wget && wget -O install.sh https://download.bt.cn/install/install_6.0.sh && sh install.sh ed8484bec
-                        elif [[ $SYSTEM_TYPE == "centos" ]]; then
-                            sudo yum install -y wget && wget -O install.sh https://download.bt.cn/install/install_6.0.sh && sh install.sh ed8484bec
-                        fi
-                        ;;
-                    2)
-                        if [[ $SYSTEM_TYPE == "debian" ]]; then
-                            sudo apt-get install -y wget && wget -O install.sh http://www.aapanel.com/script/install_6.0_en.sh && bash install.sh aapanel
-                        elif [[ $SYSTEM_TYPE == "centos" ]]; then
-                            sudo yum install -y wget && wget -O install.sh http://www.aapanel.com/script/install_6.0_en.sh && bash install.sh aapanel
-                        fi
-                        ;;
-                    3)
-                        if [[ $SYSTEM_TYPE == "debian" ]]; then
-                            sudo apt-get install -y wget && wget -O install.sh https://download.bt.cn/install/install_6.0.sh && sh install.sh uninstall
-                        elif [[ $SYSTEM_TYPE == "centos" ]]; then
-                            sudo yum install -y wget && wget -O install.sh https://download.bt.cn/install/install_6.0.sh && sh install.sh uninstall
-                        fi
-                        ;;
-                    0)
-                        break
-                        ;;
-                    *)
-                        echo -e "${RED}无效的选择${NC}"
-                        ;;
-                esac
-            done
-            ;;
-        4)
-            while true; do
-                tcp_tuning_menu
-                read -p "请选择 TCP调优 配置选项 (输入对应数字): " tcp_choice
-                case $tcp_choice in
-                    1)
-                        wget http://sh.nekoneko.cloud/tools.sh -O tools.sh && bash tools.sh
-                        ;;
-                    2)
-                        break
-                        ;;
-                    *)
-                        echo -e "${RED}无效的选择${NC}"
-                        ;;
-                esac
-            done
-            ;;
-        5)
-            while true; do
-                curl_menu
-                read -p "请选择 Curl 配置选项 (输入对应数字): " curl_choice
-                case $curl_choice in
-                    1)
-                        if [[ $SYSTEM_TYPE == "debian" ]]; then
-                            sudo apt-get update
-                            sudo apt-get install -y curl
-                        elif [[ $SYSTEM_TYPE == "centos" ]]; then
-                            sudo yum install -y curl
-                        fi
-                        ;;
-                    2)
-                        if [[ $SYSTEM_TYPE == "debian" ]]; then
-                            sudo apt-get remove -y curl
-                        elif [[ $SYSTEM_TYPE == "centos" ]]; then
-                            sudo yum remove -y curl
-                        fi
-                        ;;
-                    3)
-                        break
-                        ;;
-                    *)
-                        echo -e "${RED}无效的选择${NC}"
-                        ;;
-                esac
-            done
-            ;;
-        6)
-            while true; do
-                bbr3_menu
-                read -p "请选择 BBR3 配置选项 (输入对应数字): " bbr3_choice
-                case $bbr3_choice in
-                    1)
-                        backup_bbr
-                        bash <(curl -sSL https://raw.githubusercontent.com/WuYtUgXw/almasoga/main/bbr3.sh)
-                        ;;
-                    2)
-                        backup_bbr
-                        ;;
-                    3)
-                        restore_bbr
-                        ;;
-                    4)
-                        uninstall_bbr
-                        ;;
-                    5)
-                        break
-                        ;;
-                    *)
-                        echo -e "${RED}无效的选择${NC}"
-                        ;;
-                esac
-            done
-            ;;
-        7)
-            disable_ipv6
-            ;;
-        0)
-            echo -e "${GREEN}脚本执行完成${NC}"
-            exit 0
-            ;;
-        *)
-            echo -e "${RED}无效的选择${NC}"
-            ;;
-    esac
-done
-
-function backup_bbr {
-    if [[ $SYSTEM_TYPE == "debian" ]]; then
-        sudo cp /usr/lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko /usr/lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko.bak
-    elif [[ $SYSTEM_TYPE == "centos" ]]; then
-        sudo cp /lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko /lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko.bak
-    fi
-}
-
-function restore_bbr {
-    if [[ $SYSTEM_TYPE == "debian" ]]; then
-        sudo cp /usr/lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko.bak /usr/lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko
-    elif [[ $SYSTEM_TYPE == "centos" ]]; then
-        sudo cp /lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko.bak /lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko
-    fi
-}
-
-function uninstall_bbr {
-    if [[ $SYSTEM_TYPE == "debian" ]]; then
-        sudo rm -f /usr/lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko
-    elif [[ $SYSTEM_TYPE == "centos" ]]; then
-        sudo rm -f /lib/modules/$(uname -r)/kernel/net/ipv4/tcp_bbr/bbr.ko
-    fi
-}
-
-function disable_ipv6 {
-    if [[ $SYSTEM_TYPE == "debian" ]]; then
-        sudo sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
-        sudo sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
-        echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
-        echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
-        sudo sysctl -p
-    elif [[ $SYSTEM_TYPE == "centos" ]]; then
-        sudo sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
-        sudo sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
-        echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
-        echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
-        sudo sysctl -p
-    fi
-}
+            end
