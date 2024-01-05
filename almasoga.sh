4ef218b070586760d53061ae8732985e68bff1e5
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-05 20:50:12 +0800

    Update almasoga.sh

--- almasoga.sh
+++ almasoga.sh
@@ -16,19 +16,19 @@ echo "5. 流媒体测试"
 echo "0. 退出脚本"
 
 # 读取用户输入
-read -p "请输入功能编号：" function_number
+function_number=$(read -p "请输入功能编号：")
 
 # 根据用户输入执行相应的功能
 case $function_number in
   1)
     # Soga配置
-    read -p "请输入节点 ID：" node_id
+    node_id=$(read -p "请输入节点 ID：")
     sed -i "s/node_id=.*/node_id=$node_id/" /etc/soga/soga.conf
     systemctl restart soga
     ;;
   2)
     # 解锁配置
-    read -p "请输入地区缩写：" region_abbr
+    region_abbr=$(read -p "请输入地区缩写：")
     if [ "$region_abbr" == "hk" ]; then
       sed -i "s/sin.core/hkg.core/" /etc/soga/dns.yml
     elif [ "$region_abbr" == "sg" ]; then
@@ -42,15 +42,19 @@ case $function_number in
     ;;
   3)
     # 审计配置
-    read -p "请选择操作：" action
-    if [ "$action" == "1" ]; then
-      echo -n > /etc/soga/blockList
-    elif [ "$action" == "2" ]; then
-      curl -L -s https://raw.githubusercontent.com/WuYtUgXw/almasoga/main/blockList > /etc/soga/blockList
-    else
-      echo "无效的操作"
-      exit 1
-    fi
+    action=$(read -p "请选择操作：")
+    case $action in
+      1)
+        echo -n > /etc/soga/blockList
+        ;;
+      2)
+        curl -L -s https://raw.githubusercontent.com/WuYtUgXw/almasoga/main/blockList > /etc/soga/blockList
+        ;;
+      *)
+        echo "无效的操作"
+        exit 1
+        ;;
+    esac
     ;;
   4)
     # Soga重装
