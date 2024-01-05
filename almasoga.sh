716835a0f603f89cbf887f32a75a55746258fed5
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-05 21:14:28 +0800

    Update almasoga.sh

--- almasoga.sh
+++ almasoga.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
-# Define colors
-RED="\033[0;31m"
-YELLOW="\033[0;33m"
-GREEN="\033[0;32m"
-WHITE="\033[0;37m"
-ORANGE="\033[0;33m"
+# 定义颜色
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+yellow_light='\033[2;36m'
+plain='\033[0m'
 
-# Check for soga
+# 检测 soga
 if command -v soga &> /dev/null; then
-  # soga is installed
-  echo -e "${GREEN}soga 已安装！${WHITE}"
-  echo -e "${ORANGE}跳过安装 soga...${WHITE}"
+  # soga 已安装
+  echo "${green}soga 已安装！${plain}"
 else
-  # soga is not installed
-  echo -e "${RED}未检测到 soga，是否安装 yes or no？${WHITE}"
-  read -p "请输入：" input
-  if [ "$input" = "y" -o "$input" = "yes" ]; then
-    # Install soga
-    echo -e "${YELLOW}正在安装 soga...${WHITE}"
-    bash <(curl -sSL https://raw.githubusercontent.com/WuYtUgXw/almasoga/main/almasoga.sh)
-    echo -e "${GREEN}soga 安装成功！${WHITE}"
-  else
-    # Cancel soga installation
-    echo -e "${ORANGE}soga 未安装！${WHITE}"
-  fi
+  # soga 未安装
+  echo "${red}未检测到 soga，是否安装 yes or no？${plain}"
+
+  # 使用 select 选择器获取用户输入
+  select input in "是" "否"; do
+    case $input in
+      "是")
+        # 安装 soga
+        echo "${yellow}正在安装 soga...${plain}"
+        bash <(curl -sSL https://raw.githubusercontent.com/almasoga/almasoga/main/almasoga.sh)
+        echo "${green}soga 安装成功！${plain}"
+        break
+      ;;
+      "否")
+        # 取消安装 soga
+        echo "${orange}soga 未安装！${plain}"
+        exit
+      ;;
+      *)
+        echo "输入错误，请重新输入"
+        continue
+      ;;
+    esac
+  done
+fi
+
+# 输出版本信息
+if command -v soga &> /dev/null; then
+  echo "${green}soga 版本：${plain}"
+  soga -V
 fi
