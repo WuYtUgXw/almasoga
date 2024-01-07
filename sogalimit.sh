ce11d19b91a632263c7b859a498305c222146636
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-07 11:49:49 +0800

    Update sogalimit.sh

--- sogalimit.sh
+++ sogalimit.sh
@@ -8,4 +8,15 @@ sed -i 's/redis_enable=false/redis_enable=true/g; s/redis_addr=.*/redis_addr=red
 # 修改user_conn_limit的值为8
 sed -i 's/user_conn_limit=.*/user_conn_limit=8/g' "$config_file"
 
+# 在文件末尾添加新内容
+cat <<EOL >> "$config_file"
+dy_limit_enable=true
+dy_limit_duration=
+dy_limit_trigger_time=300
+dy_limit_trigger_speed=300
+dy_limit_speed=100
+dy_limit_time=1200
+dy_limit_white_user_id=1
+EOL
+
 echo "脚本执行完成。"
