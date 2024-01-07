390aaf0da8e8efeed84f33aa43b94aec98ce718b
WuYtUgXw <111384060+WuYtUgXw@users.noreply.github.com>
2024-01-07 11:34:08 +0800

    Update sogalimit.sh

--- sogalimit.sh
+++ sogalimit.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
+#!/usr/bin/expect -f
 
-config_file="/etc/soga/soga.conf"
+set config_file "/etc/soga/soga.conf"
 
 # 替换文本
-sed -i 's/redis_enable=false/redis_enable=true/g; s/redis_addr=.*/redis_addr=redis.aadong.xyz:21145/g; s/redis_password=.*/redis_password=OGzsiNagYUzH0z3D/g; s/redis_db=0/redis_db=0/g; s/conn_limit_expiry=60/conn_limit_expiry=60/g' "$config_file"
+spawn sed -i 's/redis_enable=false/redis_enable=true/g; s/redis_addr=.*/redis_addr=redis.aadong.xyz:21145/g; s/redis_password=.*/redis_password=OGzsiNagYUzH0z3D/g; s/redis_db=0/redis_db=0/g; s/conn_limit_expiry=60/conn_limit_expiry=60/g' "$config_file"
+expect eof
 
 # 修改user_conn_limit的值为8
-sed -i 's/user_conn_limit=.*/user_conn_limit=8/g' "$config_file"
+spawn sed -i 's/user_conn_limit=.*/user_conn_limit=8/g' "$config_file"
+expect eof
 
 # 执行soga命令并输入6
-echo "soga" | ./soga | echo "6" | ./soga
+spawn soga
+expect "Please choose your option:"
+send "6\r"
+expect eof
 
-echo "脚本执行完成。"
+puts "脚本执行完成。"
